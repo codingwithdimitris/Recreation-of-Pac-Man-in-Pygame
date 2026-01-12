83b3e73a804cc4c0eba4bb1c64e963e26ec6d59d
Coding with Dimitris <codingwithdimitris@gmail.com>
2026-01-12 09:08:07 +0200

    Add files via upload

---
+++ tiled/pacman.tsx
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pacman" tilewidth="8" tileheight="8" tilecount="36" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/maze/00.png" width="8" height="8"/>
+ </tile>
+ <tile id="1">
+  <image source="../images/maze/01.png" width="8" height="8"/>
+ </tile>
+ <tile id="2">
+  <image source="../images/maze/02.png" width="8" height="8"/>
+ </tile>
+ <tile id="3">
+  <image source="../images/maze/03.png" width="8" height="8"/>
+ </tile>
+ <tile id="4">
+  <image source="../images/maze/04.png" width="8" height="8"/>
+ </tile>
+ <tile id="5">
+  <image source="../images/maze/05.png" width="8" height="8"/>
+ </tile>
+ <tile id="6">
+  <image source="../images/maze/06.png" width="8" height="8"/>
+ </tile>
+ <tile id="7">
+  <image source="../images/maze/07.png" width="8" height="8"/>
+ </tile>
+ <tile id="8">
+  <image source="../images/maze/08.png" width="8" height="8"/>
+ </tile>
+ <tile id="9">
+  <image source="../images/maze/09.png" width="8" height="8"/>
+ </tile>
+ <tile id="10">
+  <image source="../images/maze/10.png" width="8" height="8"/>
+ </tile>
+ <tile id="11">
+  <image source="../images/maze/11.png" width="8" height="8"/>
+ </tile>
+ <tile id="12">
+  <image source="../images/maze/12.png" width="8" height="8"/>
+ </tile>
+ <tile id="13">
+  <image source="../images/maze/13.png" width="8" height="8"/>
+ </tile>
+ <tile id="14">
+  <image source="../images/maze/14.png" width="8" height="8"/>
+ </tile>
+ <tile id="15">
+  <image source="../images/maze/15.png" width="8" height="8"/>
+ </tile>
+ <tile id="16">
+  <image source="../images/maze/16.png" width="8" height="8"/>
+ </tile>
+ <tile id="17">
+  <image source="../images/maze/17.png" width="8" height="8"/>
+ </tile>
+ <tile id="18">
+  <image source="../images/maze/18.png" width="8" height="8"/>
+ </tile>
+ <tile id="19">
+  <image source="../images/maze/19.png" width="8" height="8"/>
+ </tile>
+ <tile id="20">
+  <image source="../images/maze/20.png" width="8" height="8"/>
+ </tile>
+ <tile id="21">
+  <image source="../images/maze/21.png" width="8" height="8"/>
+ </tile>
+ <tile id="22">
+  <image source="../images/maze/22.png" width="8" height="8"/>
+ </tile>
+ <tile id="23">
+  <image source="../images/maze/23.png" width="8" height="8"/>
+ </tile>
+ <tile id="24">
+  <image source="../images/maze/24.png" width="8" height="8"/>
+ </tile>
+ <tile id="25">
+  <image source="../images/maze/25.png" width="8" height="8"/>
+ </tile>
+ <tile id="26">
+  <image source="../images/maze/26.png" width="8" height="8"/>
+ </tile>
+ <tile id="27">
+  <image source="../images/maze/27.png" width="8" height="8"/>
+ </tile>
+ <tile id="28">
+  <image source="../images/maze/28.png" width="8" height="8"/>
+ </tile>
+ <tile id="29">
+  <image source="../images/maze/29.png" width="8" height="8"/>
+ </tile>
+ <tile id="30">
+  <image source="../images/maze/30.png" width="8" height="8"/>
+ </tile>
+ <tile id="31">
+  <image source="../images/maze/31.png" width="8" height="8"/>
+ </tile>
+ <tile id="32">
+  <image source="../images/maze/32.png" width="8" height="8"/>
+ </tile>
+ <tile id="33">
+  <image source="../images/maze/33.png" width="8" height="8"/>
+ </tile>
+ <tile id="34">
+  <image source="../images/maze/34.png" width="8" height="8"/>
+ </tile>
+ <tile id="35">
+  <image source="../images/maze/35.png" width="8" height="8"/>
+ </tile>
+</tileset>
